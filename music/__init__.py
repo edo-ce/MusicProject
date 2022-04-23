2c563909df10442c7310889f2791b3f992d2b773
Mattia Dei Rossi <mattydr98@gmail.com>
2022-04-23 15:14:28 +0200

    Added tables

--- music/__init__.py
+++ music/__init__.py
@@ -2,10 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/musicsql'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:ferrari98@localhost/musicsql'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config['SECRET_KEY'] = 'secret_key'
 
-#db = SQLAlchemy(app)
+db = SQLAlchemy(app)
+db.create_all()
 
 from music import routes
--- music/models.py
+++ music/models.py
@@ -1,36 +1,71 @@
-#from music import db
+from music import db
 from flask_login import UserMixin
 
-# TODO mancano tabelle
+# ------------------------------------------------------
+class User(db.Model):
+    email = db.Column(db.String, primary_key=True)
+    password = db.Column(db.String(8))
+    username = db.Column(db.String)
+    name = db.Column(db.String)
+    surname = db.Column(db.String)
+    gender = db.Column(db.String)
+    country = db.Column(db.String)
+    birth_date = db.Column(db.Date)
+
+class Artist(User):
+    stage_name = db.Column(db.String)
+    is_solo = db.Column(db.Boolean)
+    bio = db.Column(db.String)
+
+class Listener(User):
+    entry_date = db.Column(db.Date)
+    is_premium = db.Column(db.Boolean)
+
+# ------------------------------------------------------
+class Elements(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+
+class Album(Elements):
+    release_date = db.Column(db.Date)
+
+class Track(Elements):
+    duration = db.Column(db.String)
+    genre = db.Column(db.String)
+
+class Playlists(Elements):
+    is_private = db.Column(db.Boolean)
+    maker = db.Column(db.String)
+
+#-----------------------------------------------------
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.Date)
+    start_time = db.Column(db.Date)
+    end_time = db.Column(db.Date)
+    location = db.Column(db.String)
+    link = db.Column(db.String)
+
+class Genre(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+
+class PaymentCard(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    number = db.Column(db.String)
+    security_code = db.Column(db.String)
+    expiration_date = db.Column(db.Date)
+    owner = db.Column(db.String)
+    type = db.Column(db.String)
+
+#----------------------------------------------------
+class Premium(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    registration_date = db.Column(db.Date)
+
+class Follower(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    following_data = db.Column(db.Date)
 
 
-class User:
-    pass
 
-
-class Artist:
-    pass
-
-
-class Listener:
-    pass
-
-
-class Album:
-    pass
-
-
-class Song:
-    pass
-
-
-class Playlist:
-    pass
-
-
-class Event:
-    pass
-
-
-class Genre:
-    pass
