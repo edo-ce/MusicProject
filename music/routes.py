2c5817a056fe85402da36fdc6e3f7b06b4e570cb
Edo <edoardocecchinato.ec@gmail.com>
2022-04-26 23:10:19 +0200

    Added premium route

--- music/routes.py
+++ music/routes.py
@@ -1,7 +1,7 @@
 from music import app, db, bcrypt
 from flask import render_template, redirect, url_for, flash
-from music.models import User
-from music.forms import SignUpForm, LoginForm
+from music.models import User, PaymentCard
+from music.forms import SignUpForm, LoginForm, PaymentForm
 from flask_login import login_user, logout_user, login_required
 
 
@@ -32,18 +32,36 @@ def login():
 
 
 @app.route('/logout')
+@login_required
 def logout():
     logout_user()
     flash('You have been logged out! See you soon', category='info')
     return redirect(url_for('home'))
 
 
+@app.route('/premium')
+@login_required
+def premium():
+    form = PaymentForm()
+    if form.validate_on_submit():
+        card = PaymentCard(number=form.number.data, security_code=form.pin.data,
+                           expiration_date=form.expiration_date.data,
+                           owner=form.holder.data, type=form.type.data)
+        db.session().add(card)
+        db.session.commit()
+        flash('Account Premium create successfully', category='success')
+        return redirect(url_for('private'))
+    return render_template('premium.html', form=form)
+
+
 @app.route('/private')
+@login_required
 def private():
     return render_template('private_artist.html')
 
 
 @app.route('/search')
+@login_required
 def search():
     return render_template('search.html')
 
