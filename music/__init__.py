a515f8808baa7ed25dac67c8c23f75dd956f8653
Mattia Dei Rossi <mattydr98@gmail.com>
2022-04-26 22:31:16 +0200

    read args from commandline 'run.py password_db databse_name'

--- music/__init__.py
+++ music/__init__.py
@@ -1,10 +1,12 @@
+import sys
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:ferrari98@localhost/musicsql'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://postgres:{sys.argv[1]}@localhost/{sys.argv[2]}'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config['SECRET_KEY'] = 'secret_key'
 
