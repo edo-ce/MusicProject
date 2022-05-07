af3b5fdb1632719e8e63f861a39e429aef5423c8
Mattia Dei Rossi <mattydr98@gmail.com>
2022-05-07 13:41:43 +0200

    upload data from MusiBrainz to db

--- music/test/populate.py
+++ music/test/populate.py
@@ -5,6 +5,10 @@
 import musicbrainzngs
 import sys
 import json
+from music.models import *
+from datetime import *
+
+
 
 # online db
 musicbrainzngs.set_useragent(
@@ -12,7 +16,6 @@ musicbrainzngs.set_useragent(
     "0.1",
     "https://github.com/alastair/python-musicbrainzngs/",
 )
-
 
 if __name__ == '__main__':
     # cath artist and album from cmdline
@@ -22,18 +25,49 @@ if __name__ == '__main__':
     artist, album=args
 
     #query result
-    tracks = musicbrainzngs.search_recordings(artist=artist, release=album, limit=5)
+    tracks = musicbrainzngs.search_recordings(artist=artist, release=album, limit=10)
 
     #print(json.dumps(tracks.get('recording-list')[0].get('title'), indent=4, sort_keys=True))
-    #printing title of tracks
+
+    # Genere
+    genre = Genre(name="Rap")
+    # Users
+    user_obj = User(username="tt0", name="Davide", lastname="De Luca", gender="M", country="Italy", birth_date=str(datetime.now()), email="gemi@gemi.com", password="password" )
+    # Artist
+    artist_obj = Artist(id=user_obj.username, stage_name="Gemitaiz", is_solo=True, bio="gemi Bio")
+    # Album
+    album_element = Element(title=album)
+    album_obj = Album(release_date=date.today(), artist_id=artist_obj.id, id=album_element.id, )
+
+    #containers
+    element_vect = []
+    track_vect = []
+
+    #add tracks
     for id, track in enumerate (tracks.get('recording-list')):
-        print(id, '{}'.format( track['title'] ) )
+
+        print(id, '{}'.format( track ) )
+        element_vect.append( Element( title= track['title'] ) )
+        track_vect.append( Track( id=element_vect[-1].id, duration= track['length'], copyright="example",genre=genre,album_id=album_obj.id))
 
 
-    #from music import base, engine, session
-    #from music.models import *
-    #Base.metadata.create_all(engine)
+    #session.query(Element).delete()
 
-    #for id, track in enumerate(tracks.get('recording-list')):
-    #    print(track['title'])
+
+    #upload elements
+    try:
+        session.add(user_obj)
+        session.add(artist_obj)
+        session.add(album_element)
+        session.add(album_obj)
+        session.add(genre)
+
+        session.add_all(element_vect)
+        session.add_all(track_vect)
+    except Exception as ex:
+        print(ex)
+        session.rollback()
+        print ("rollback")
+
+    session.commit()
 
