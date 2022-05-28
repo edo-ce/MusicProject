707037ac1b9610b4f19f2c4e3a80346e6dc6109b
Mattia Dei Rossi <mattydr98@gmail.com>
2022-05-28 18:00:25 +0200

    2 trigger plpgsql

--- music/test/trigger.sql
+++ music/test/trigger.sql
@@ -1,16 +1,19 @@
+/* 2) */
 CREATE TRIGGER check_saved_album
 BEFORE INSERT OR UPDATE
 ON saved_elements
-FOR EACH ROW
+FOR EACH STATEMENT
 EXECUTE FUNCTION check_saved_album();
 
 CREATE FUNCTION check_saved_album() RETURNS trigger AS $$
 BEGIN
-    IF NEW.idElemento = (SELECT idElemento
-                         FROM
-                         WHERE
+    IF (5 < (   SELECT COUNT(*)
+                FROM Listener JOIN saved_album
+                WHERE NEW.id_listener = Listener.id ) THEN
+        RETURN NEW;
 
-
+    END IF;
+    RETURN NULL;
 
 END;
 $$ LANGUAGE plpgsql;
