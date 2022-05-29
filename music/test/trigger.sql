c2dacba546a54a9e7c32d1093689b6e164513457
Mattia Dei Rossi <mattydr98@gmail.com>
2022-05-29 11:26:12 +0200

    fixed trigger 2)

--- music/test/trigger.sql
+++ music/test/trigger.sql
@@ -2,14 +2,19 @@
 CREATE TRIGGER check_saved_album
 BEFORE INSERT OR UPDATE
 ON saved_elements
-FOR EACH STATEMENT
+FOR EACH ROW
 EXECUTE FUNCTION check_saved_album();
 
 CREATE FUNCTION check_saved_album() RETURNS trigger AS $$
 BEGIN
+    IF ( EXISTS( SELECT *
+                 FROM Premiums
+                 WHERE NEW.id_listener = Premiums.id ) ) THEN
+        RETURN NEW;
+    END IF;
     IF (5 < (   SELECT COUNT(*)
-                FROM Listener JOIN saved_album
-                WHERE NEW.id_listener = Listener.id ) THEN
+                FROM saved_elements
+                WHERE (NEW.id_listener = saved_elements.id_listener ) THEN
         RETURN NEW;
 
     END IF;
