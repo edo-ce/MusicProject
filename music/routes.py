2655676786e24104ead2b8f08232d9506c22e4b9
Mattia Dei Rossi <mattydr98@gmail.com>
2022-06-28 23:00:03 +0200

    no cache saving

--- music/routes.py
+++ music/routes.py
@@ -7,6 +7,14 @@ from music.algorithms import *
 from datetime import date
 from sqlalchemy.exc import InternalError
 
+# no cache saving
+@app.after_request
+def add_header(r):
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
 
 @app.errorhandler(IntegrityError)
 def handle_error(e):
